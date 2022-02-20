c85d0d322fccb2eaddfbfd5199892656e266ed7d
Ilmir <valitovilmir29@gmail.com>
2022-02-20 18:24:48 +0300

    1) del class Logging and added a prefix "LOG_" for variables 2) Edited main(), fixed repeated message and used method "Guard block" 3) message "Error" is saved only after the message is sent
    Fixed pip8

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 600
+RETRY_TIME = 6
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -33,27 +33,28 @@ handler = RotatingFileHandler(
     encoding='utf-8')
 logger.addHandler(handler)
 
-SEND_SUCCESSFUL = 'Успешная отправка сообщения: '
-SEND_ERROR = 'Ошибка при отправке сообщения: '
-CONNECTION_TRY = 'Попытка запроса к: '
-CONNECTION_SUCCESSFUL = 'Успешный запрос с '
-CONNECTION_ERROR = 'Ошибка при запросе: '
-CODE_ERROR = '\n Код ошибки: '
-KEY_ERROR = 'Не найден ключ: '
-NOT_HOMEWORKS = 'В ответе сервера не нашел: '
-NOT_TOKEN = 'Нет обязательной переменной для запуска программы'
-LOOP_REPEAT = f'Бот делает повторный запрос, после {RETRY_TIME}с. сна'
-NO_STATUS_CHANGE = f'Статусы не изменены, повторный запуск через {RETRY_TIME}'
+LOG_SEND_SUCCESSFUL = 'Успешная отправка сообщения: '
+LOG_SEND_ERROR = 'Ошибка при отправке сообщения: '
+LOG_CONNECTION_TRY = 'Попытка запроса к: '
+LOG_CONNECTION_SUCCESSFUL = 'Успешный запрос с '
+LOG_CONNECTION_ERROR = 'Ошибка при запросе: '
+LOG_CODE_ERROR = '\n Код ошибки: '
+LOG_KEY_ERROR = 'Не найден ключ: '
+LOG_NOT_HOMEWORKS = 'В ответе сервера не нашел: '
+LOG_NOT_TOKEN = 'Нет обязательной переменной для запуска программы'
+LOG_LOOP_REPEAT = f'Бот делает повторный запрос, после {RETRY_TIME}с. сна'
+LOG_NO_STATUS_CHANGED = (f'Статусы не изменены, '
+                         f'повторная проверка через {RETRY_TIME}с.')
 
 
 def send_message(bot: telegram.Bot, message: str) -> requests.request:
     """Отправляет сообщение в Telegram чат."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-        logger.info(SEND_SUCCESSFUL + message)
+        logger.info(LOG_SEND_SUCCESSFUL + message)
     except ConnectionError:
-        logger.error(SEND_ERROR + message)
-        raise ConnectionError(SEND_ERROR + message)
+        logger.error(LOG_SEND_ERROR + message)
+        raise ConnectionError(LOG_SEND_ERROR + message)
 
 
 def get_api_answer(current_timestamp: int) -> requests.get:
@@ -62,30 +63,29 @@ def get_api_answer(current_timestamp: int) -> requests.get:
     params = {'from_date': timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        logger.info(CONNECTION_SUCCESSFUL + ENDPOINT)
+        logger.info(LOG_CONNECTION_SUCCESSFUL + ENDPOINT)
         if response.status_code == HTTPStatus.OK:
-            logger.info(CONNECTION_SUCCESSFUL + ENDPOINT)
+            logger.info(LOG_CONNECTION_SUCCESSFUL + ENDPOINT)
             return response.json()
     except ConnectionError:
-        logger.error(CONNECTION_ERROR + ENDPOINT)
-        raise ConnectionError(CONNECTION_ERROR + ENDPOINT)
-    else:
-        logger.error(CONNECTION_ERROR + ENDPOINT
-                     + CODE_ERROR + str(response.status_code))
-        raise ConnectionError(CONNECTION_ERROR + ENDPOINT
-                              + CODE_ERROR + str(response.status_code))
+        logger.error(LOG_CONNECTION_ERROR + ENDPOINT)
+        raise ConnectionError(LOG_CONNECTION_ERROR + ENDPOINT)
+    logger.error(LOG_CONNECTION_ERROR + ENDPOINT
+                 + LOG_CODE_ERROR + str(response.status_code))
+    raise ConnectionError(LOG_CONNECTION_ERROR + ENDPOINT
+                          + LOG_CODE_ERROR + str(response.status_code))
 
 
 def check_response(response: requests.request) -> list:
     """Проверяет ответ API на корректность."""
     KEY = 'homeworks'
     if KEY not in response:
-        logger.error(KEY_ERROR, KEY)
-        raise KeyError(KEY_ERROR, KEY)
+        logger.error(LOG_KEY_ERROR, KEY)
+        raise KeyError(LOG_KEY_ERROR, KEY)
     if isinstance(response[KEY], list):
         return response[KEY]
-    logger.error(NOT_HOMEWORKS + KEY)
-    raise TypeError(NOT_HOMEWORKS + KEY)
+    logger.error(LOG_NOT_HOMEWORKS + KEY)
+    raise TypeError(LOG_NOT_HOMEWORKS + KEY)
 
 
 def parse_status(homework: requests.request) -> str:
@@ -93,8 +93,8 @@ def parse_status(homework: requests.request) -> str:
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
     if homework_status not in VERDICTS:
-        logger.error(KEY_ERROR + homework_status)
-        raise KeyError(KEY_ERROR + homework_status)
+        logger.error(LOG_KEY_ERROR + homework_status)
+        raise KeyError(LOG_KEY_ERROR + homework_status)
     verdict = VERDICTS[homework_status]
     logger.info(f'Успешно извлекли и передали: \n'
                 f' имя: "{homework_name}", \n'
@@ -107,7 +107,7 @@ def check_tokens() -> bool:
     all_token = (PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
     for TOKEN in all_token:
         if TOKEN is None:
-            logger.critical(NOT_TOKEN + str(TOKEN))
+            logger.critical(LOG_NOT_TOKEN + str(TOKEN))
             return False
     return True
 
@@ -115,37 +115,37 @@ def check_tokens() -> bool:
 def main() -> None:
     """Основная логика работы бота."""
     if not check_tokens():
-        logger.critical(NOT_TOKEN)
-        raise Warning(NOT_TOKEN)
+        logger.critical(LOG_NOT_TOKEN)
+        raise Warning(LOG_NOT_TOKEN)
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    old_homeworks_or_error = [None]
+    last_error = [None]
+    last_message = {}
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
-            if old_homeworks_or_error[0] != homeworks:
-                for homework in homeworks:
-                    for old_homework in old_homeworks_or_error:
-                        if homework != old_homework:
-                            status_homework = parse_status(homework)
-                            send_message(bot, status_homework)
-            old_homeworks_or_error[0] = homeworks
+            for homework in homeworks:  # Лучше ничего не смог придумать((
+                if homework['homework_name'] not in last_message:
+                    status_homework = parse_status(homework)
+                    send_message(bot, status_homework)
+                    last_message[homework['homework_name']] = status_homework
+
             current_timestamp = 1
             time.sleep(RETRY_TIME)
-            logger.info(LOOP_REPEAT, exc_info=True)
-            logger.debug(NO_STATUS_CHANGE)
+            logger.info(LOG_LOOP_REPEAT, exc_info=True)
+            logger.debug(LOG_NO_STATUS_CHANGED)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            if old_homeworks_or_error[0] != message:
+            if last_error[0] != message:
                 send_message(bot, message)
-                logger.error(CONNECTION_ERROR + str(error), exc_info=True)
-            old_homeworks_or_error[0] = message
+                last_error[0] = message
+                logger.error(LOG_CONNECTION_ERROR + str(error), exc_info=True)
             time.sleep(RETRY_TIME)
         else:
             time.sleep(RETRY_TIME)
-            logger.info(LOOP_REPEAT, exc_info=True)
+            logger.info(LOG_LOOP_REPEAT, exc_info=True)
 
 
 if __name__ == '__main__':
